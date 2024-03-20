d08ca0880e78a792dd5b31e1e268c33eeb51806e
kundinasai <kundinasai43@gmail.com>
2024-03-20 11:37:42 +0530

    commit 1234

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+// import { UserContext } from '../App'
+import { Usecontext1 } from "./UserContext/Usercontext1";
+//import { srikanth } from '../UserContext/Usercontext1';
+import Home from "./Componenta/Home";
+import Login from "./Componenta/Login";
+import Register from "./Componenta/Register";
 
-function App() {
+import Feeds from "./Componenta/Feeds";
+import Posts from "./Componenta/Posts";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Albums from "./Componenta/Albums";
+import Comments from "./Componenta/Comments";
+
+export default function App() {
+  const { flag, setFlag } = useContext(Usecontext1);
+  const PATH = process.env.REACT_APP_PATH;
+  //const {flag,setFlag} = useContext(srikanth)
+  //const [flag,setFlag] = useState()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      {flag == 0 || flag == 2 ? <Login /> : <Home />}
+      
+      {flag === 2 && <Register />}
+
+      
+        <Routes>
+          <Route index path={`${PATH}/`}  element={<Feeds />}></Route>
+          <Route path={`${PATH}/post`} element={<Posts />}></Route>
+          <Route path={`${PATH}/albums`} element={<Albums />}></Route>
+          <Route path={`${PATH}/comments`} element={<Comments/>}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
-
-export default App;
